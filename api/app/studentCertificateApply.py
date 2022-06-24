f30032cc66f5973c05a3275b85e5569f995fb8e6
huangyongjun <465469264@qq.com>
2022-06-24 15:57:54 +0800

    优化了申请报读

--- api/app/studentCertificateApply.py
+++ api/app/studentCertificateApply.py
@@ -56,10 +56,53 @@ class studentCertificateApply(HttpRunner):
                             })
                 .with_data('$data')
                 .extract()
-                .with_jmespath("body.body[0].checkStatus","$checkStatus")
                 .validate()
                 .assert_equal("status_code", 200)
         )
     ]
 if __name__ == '__main__':
     studentCertificateApply().test_start()
+
+
+class studentCertificateApply2(HttpRunner):
+    config = (
+        Config("申请证明")
+            .base_url("${ENV(app_BASE_URL)}")
+            .verify(False)
+            .variables(**{
+                            "number": {
+                                    "body":{
+                                            "applyType": "$applyType",  # 6>报读证明  5>其他       3>查询
+                                            "remark": "$remark",                #备注
+                                            "stdName": "$stdName",
+                                            "receiveType": "$receiveType",             #未知字段
+                                            "unvsName": "$unvsName",
+                                            "userId": "$userId",
+                                            "applyPurpose": "$applyPurpose",            #申请理由
+                                            "grade": "$grade",
+                                            "learnId": "$learnId"
+                                            },
+                                    "header":{
+                                        "appType":"3"
+                                    }
+                                },
+                            "data": "${base64_encode($number)}"
+                        })
+            )
+    teststeps = [
+        Step(
+            RunRequest("申请证明")
+                .post("/proxy/bds/studentCertificateApply/1.0/")
+                .with_headers(**{
+                "User-Agent": "Android/environment=test/app_version=7.19.9/sdk=28/dev=samsung/phone=SM-N9500/android_system=9",
+                "frontTrace": "{\"transferSeq\":\"1\",\"phoneModel\":\"SM-N9500\",\"app_type\":\"android\",\"app_version\":\"7.19.9\",\"title\":\"getCertificateApply\",\"transferId\":\"165596882382164439\",\"uri\":\"/proxy/bds/getCertificateApply/1.0/\",\"phoneSys\":\"9\",\"app_sdk\":\"28\",\"sendTime\":\"1655968823822\"}",
+                "Content-Type": "text/yzedu+; charset=UTF-8",
+                "Host": "${ENV(app_Host)}",
+                "authtoken": "$app_auth_token",
+                            })
+                .with_data('$data')
+                .extract()
+                .validate()
+                .assert_equal("status_code", 200)
+        )
+    ]
--- testcases/main_process/wo_de/test_3Apply_enrollment.py
+++ testcases/main_process/wo_de/test_3Apply_enrollment.py
@@ -1,7 +1,8 @@
 from httprunner import HttpRunner, Config, Step, RunRequest,RunTestCase
 from api.app.loginOrRegister import app_login
 from api.app.stdLearnInfo import stdLearnInfo
-from api.app.studentCertificateApply import studentCertificateApply
+from api.app.studentCertificateApply import studentCertificateApply2
+from api.app.getCertificateApply import getCertificateApply
 
 
 class Test_Apply_Enrollment(HttpRunner):
@@ -15,40 +16,17 @@ class Test_Apply_Enrollment(HttpRunner):
     teststeps = [
         Step(RunTestCase("登录申请学员报读的手机号").call(app_login).export(*["app_auth_token","userId"])),
         Step(RunTestCase("获取学员报读信息").call(stdLearnInfo).export(*["learnId","std_name","unvsId","unvsName","grade"])),
-        Step(RunTestCase("申请报读证明").with_variables(**({"stdName":"$std_name","userId":"$unvsId","applyType":"6","remark":"测试","applyPurpose":"测试","receiveType":"3","unvsName":"",
-                                                                 "districtCode": "","district": "","city": "",
-                                                                "cityCode": "","stampDown": "","receiveName": "",
-                                                                "provinceCode": "","province": "","receiverName": "",
-                                                                    "receiverMobile": "",
-                                                                 "receiveMobile": "","receiveAddress": "","receiverAddress": "","materialName":"",
-                                                            })).call(studentCertificateApply)),
+        Step(RunTestCase("申请报读证明").with_variables(**({"stdName":"$std_name","userId":"$unvsId","applyType":"6","remark":"测试",
+                                                      "applyPurpose":"测试","receiveType":"3","unvsName":""})).call(studentCertificateApply2)),
+        Step(RunTestCase("申请列表,其他申请证明为已完成").with_variables(**({"a": "0", "checkStatus": 3})).call(getCertificateApply)),
 
-     # 异常传参
-        # Step(RunTestCase("申请报读证明，不传备注").with_variables(**({"stdName": "$std_name", "userId": "$unvsId", "applyType": "6", "remark": "", "applyPurpose": "测试","receiveType": "3", "unvsName": "",
-        #                                                            "districtCode": "", "district": "", "city": "",
-        #                                                            "cityCode": "", "stampDown": "", "receiveName": "",
-        #                                                            "provinceCode": "", "province": "", "receiverName": "",
-        #                                                            "receiverMobile": "",
-        #                                                            "receiveMobile": "", "receiveAddress": "",
-        #                                                            "receiverAddress": "", "materialName": "",
-        #                                                             })).call(studentCertificateApply)),
-        # Step(RunTestCase("申请报读证明，不传备注,不传申请用途").with_variables(**({"stdName": "$std_name", "userId": "$unvsId", "applyType": "6", "remark": "", "applyPurpose": "","receiveType": "3", "unvsName": "",
-        #                                                           "districtCode": "", "district": "", "city": "",
-        #                                                           "cityCode": "", "stampDown": "", "receiveName": "",
-        #                                                           "provinceCode": "", "province": "",
-        #                                                           "receiverName": "",
-        #                                                           "receiverMobile": "",
-        #                                                           "receiveMobile": "", "receiveAddress": "",
-        #                                                           "receiverAddress": "", "materialName": "",
-        #                                                           })).call(studentCertificateApply)),
-        # Step(RunTestCase("申请报读证明，输入类型").with_variables(**({"stdName":"$std_name","userId":"$unvsId","applyType":"6","remark":"测试@@@123古典风格fffff","applyPurpose":"测试@@@123古典风格fffff测试@@@123古典风格fffff测试@@@123古典风格fffff","receiveType":"3","unvsName":"",
-        #                                                    "districtCode": "", "district": "", "city": "",
-        #                                                    "cityCode": "", "stampDown": "", "receiveName": "",
-        #                                                    "provinceCode": "", "province": "", "receiverName": "",
-        #                                                    "receiverMobile": "",
-        #                                                    "receiveMobile": "", "receiveAddress": "",
-        #                                                    "receiverAddress": "", "materialName": "",
-        #                                                    })).call(studentCertificateApply)),
+        # 异常传参
+        Step(RunTestCase("申请报读证明，不传备注").with_variables(**({"stdName": "$std_name", "userId": "$unvsId", "applyType": "6", "remark": "",
+                                                           "applyPurpose": "测试","receiveType": "3", "unvsName": "",})).call(studentCertificateApply2)),
+        Step(RunTestCase("申请报读证明，不传备注,不传申请用途").with_variables(**({"stdName": "$std_name", "userId": "$unvsId", "applyType": "6", "remark": "",
+                                                                  "applyPurpose": "","receiveType": "3", "unvsName": "",})).call(studentCertificateApply2)),
+        Step(RunTestCase("申请报读证明，输入类型").with_variables(**({"stdName":"$std_name","userId":"$unvsId","applyType":"6","remark":"测试@@@123古典风格fffff",
+                                                           "applyPurpose":"测试@@@123古典风格fffff测试@@@123古典风格fffff测试@@@123古典风格fffff","receiveType":"3","unvsName":"",})).call(studentCertificateApply2)),
 
     ]
 if __name__ == '__main__':
